00f738a81dfc3f4ab5c287853ca5594acc096de0
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-02-23 13:52:16 +0330

    lern with freecodcamp

---
+++ index.js
@@ -0,0 +1,18 @@
+// Use Bracket Notation to Find the Last Character in a String
+const lastName = "love you";
+
+const lastLetterOfLastName = lastName[lastName.length - 1];
+console.log(lastLetterOfLastName);
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+console.log(secondToLastLetterOfLastName);
+// Word Blanks
+// In JavaScript, strings can be concatenated using the + operator to form a complete sentence.
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+const wordBlanks =
+  "the" + " " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+console.log(wordBlanks);
+// توجه داشته باشم که داخل کوتیشن ها نباید بنویسم چون استرینگ میشه و درست اعمال نمشه
