4a5f29692bfa2df54fd7c19fe64dafc4341f3ecf
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-03-03 21:40:17 +0330

    exaceier opshnal

--- homeworkjs4/js-4-homeworks/OPTIONAL/index.js
+++ homeworkjs4/js-4-homeworks/OPTIONAL/index.js
@@ -4,16 +4,36 @@
 //todo-3. Is it an equilateral triangle?
 //todo-4.Is it a right triangle ?
 
-let angle1;
-let angle2;
-let angle3;
+let angle1 = "80";
+let angle2 = "50";
+let angle3 = "50";
+
+if (+angle1 + +angle2 + +angle3 === 180) {
+    if (
+        angle1.includes("60") === true &&
+        angle2.includes("60") === true &&
+        angle3.includes("60") === true
+    ) {
+        console.log("equilateral triangle");
+    } else if(
+    angle1.includes("90") === true ||
+    angle2.includes("90") === true ||
+    angle3.includes("90") === true)
+    {
+        console.log("right triangle");
+    }else{
+        console.log("this is triangle");
+    }
+} else {
+  console.log("Are you shuer?");
+}
 
 //Q-2:Which one is truthy ?
 /* 
 
 1) ''
 2) ""
-3) "false"
+3) "false"  //this
 4) 0
 
 */
