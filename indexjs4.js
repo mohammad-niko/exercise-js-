2a745bf9b2af86091a6e6aed4e6ae4760a8fabfa
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-03-15 15:38:55 +0330

    execerse loop for

--- indexjs4.js
+++ indexjs4.js
@@ -19,11 +19,48 @@
 //     console.log(i);
 // }
 
-const thing = ["laptap", "phone", "charger", "tablet", "another thing"];
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log( sum += numbers[i]);
 
-for (let i = 0; i < thing.length; i++) {
-  console.log(thing[i]);
-  for (let c = 0; c < 10; c ++) {
-    console.log(`some number saler ${c}`);
-  }
+// }
+
+// let names = ["Ali", "Sara", "Reza", "Mina"];
+
+// for (let i = names.length -1 ; i >= 0 ; i--){
+//   console.log(names[i]);
+// }
+
+// let cities = ["tehran", "mashhad", "shiraz"];
+// let uperCase = [];
+
+// for (let i = 0; i < cities.length; i++) {
+//   uperCase.push(cities[i].toUpperCase());
+// }
+// console.log(uperCase);
+
+// let shab = "In shabi ke migan shab nist, age shabe mese dishab nist";
+// for (let i = 0; i < 6; i++) {
+//   console.log(shab);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i} * ${i} = ${i * i}`);
+//   for (let c = 0; c <= 10; c++) {
+//     console.log(`${c} * ${i} = ${c * i}`);
+//   }
+// }
+
+let num = [1,2,3,4];
+for (let i = 0; i < 1; i++) {
+  console.log(num.reverse());
 }
