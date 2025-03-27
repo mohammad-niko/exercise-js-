2fcbab2f21d59de1e69230dbe193c3d02312e7ee
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-03-27 14:48:23 +0330

    code wars kuta 6

--- codwarsjs.js
+++ codwarsjs.js
@@ -45,7 +45,6 @@
 
 //     if ( n < 0 || n ===0 || m < 0 || m ===0) return "INVALID"
 
-
 //   let num = 0;
 //   for (let i = n; i < m; i += n) {
 //     num = num += i;
@@ -88,7 +87,7 @@
 //         arr.push(x[i] + 2 ) ;
 //     }else {arr.push(x[i] *2);
 
-//     } 
+//     }
 //   }
 //     return arr
 //   }
@@ -97,3 +96,21 @@
 //   function maps(x){
 // x.map(i => i*2)
 //   }
+// ------------------------------------------------------------------------------------------------------------------------------------------
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+  if (number < 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (number % 3 === 0 ||number % 5 === 0 ) {
+      sum += i
+    } 
+  }
+}
