fbdf9e113f2055f81d232bce8c57c5a016512221
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-03-29 00:05:06 +0330

    holiday homeworks

--- holidays-homeworks/holidays-homeworks/quiz1.js
+++ holidays-homeworks/holidays-homeworks/quiz1.js
@@ -4,3 +4,63 @@
 vowels => a e i o u
 Example ===> "Hello" = 2
 */
+
+// function vowels(word) {
+//   let arr = [];
+//   for (let i = 0; i < word.length; i++) {
+//     switch (word[i]) {
+//       case "a":
+//         arr.push(word[i]);
+//         break;
+//       case "e":
+//         arr.push(word[i]);
+//         break;
+//       case "i":
+//         arr.push(word[i]);
+//         break;
+//       case "o":
+//         arr.push(word[i]);
+//         break;
+//       case "u":
+//         arr.push(word[i]);
+//         break;
+//     }
+//   }
+//   return `${word} = ${arr.length}`;
+// }
+
+// function vowels(word) {
+//   let arr = [];
+//   for (let i = 0; i < word.length; i++) {
+//     if (
+//       word[i] === "a" ||
+//       word[i] === "e" ||
+//       word[i] === "i" ||
+//       word[i] === "o" ||
+//       word[i] === "u"
+//     ) {
+//       arr.push(word[i]);
+//     }
+//   }
+//   return `${word} = ${arr.length}`;
+// }
+
+// function vowels(word) {
+//   let count = 0;
+//   let vowels = "a e i o u A E I O U";
+
+//   for (let char of word){
+//     if (vowels.includes(char)){
+//         count++
+//     }
+//   }
+//   return `${word} = ${count}`;
+// }
+
+function vowels(word) {
+  let vowels = "a e i o u A E I O U";
+  let count = word.split("").filter((chr) => vowels.includes(chr)).length;
+  return `${word} = ${count}`;
+}
+
+console.log(vowels("JavaScript"));
--- holidays-homeworks/holidays-homeworks/quiz2.js
+++ holidays-homeworks/holidays-homeworks/quiz2.js
@@ -3,3 +3,21 @@
 Example ===> [[1, 2, 3], [4, 5, 6], [7, 8, 9]] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 */
 // with .map()
+
+
+console.log(Falt([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+
+function Falt(towDarray){
+    let arr = [];
+    let oneDArray = towDarray.join(",");
+
+let rezlt =  oneDArray.split(",").map(chr => arr.push(parseInt(chr)))
+
+    return arr
+}
+
+
+// function Falt(towDarray){
+//      let oneDArray = towDarray.flat()
+//      return  oneDArray
+// }
