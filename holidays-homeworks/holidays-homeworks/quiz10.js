087f004b4d438fad6da87117bdd248cd4b04c0aa
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-04-01 22:41:22 +0330

    holiday homework

--- holidays-homeworks/holidays-homeworks/quiz10.js
+++ holidays-homeworks/holidays-homeworks/quiz10.js
@@ -3,12 +3,17 @@
 Example ===> "abcdabc" = d
 */
 
+function remove(chr) {
+//   let arr = chr.split("");
+//   let rezult = [];
+//   for ( let i = 0 ; i < arr.length ; i++ ){
+//     if(chr.indexOf(arr[i]) === chr.lastIndexOf(arr[i])){
+//         rezult.push(arr[i])
+//     }
+//   }
+//   return rezult
+// =============================================================================================================================
+return  chr.split("").filter(item => chr.indexOf(item)===chr.lastIndexOf(item))
+}
 
-// function remove(chr){
-//     let arr = chr.split("").filter((item , index) =>chr.indexOf(item)===index )
-//     return arr
-// }
-
-// console.log(remove("abcdabc"));
-
-
+console.log(remove("abcdmabc"));
